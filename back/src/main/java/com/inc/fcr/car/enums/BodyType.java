8258f101632cf7f1d3f155f175314caf916d9afa
Jenny <jenny.ventura.447@my.csun.edu>
2026-03-08 00:01:36 -0800

    Added 2 new enum files (BodyType and RoofType) and added 4WD in Drivetrain. If we don't need them, that's fine.

---
+++ back/src/main/java/com/inc/fcr/car/enums/BodyType.java
@@ -0,0 +1,5 @@
+package com.inc.fcr.car.enums;
+
+public enum BodyType {
+    SEDAN, SUV, PICKUP TRUCK, CONVERTABLE, HATCHBACK, VAN, COMPACT, WAGON
+}
---
+++ back/src/main/java/com/inc/fcr/car/enums/RoofType.java
@@ -0,0 +1,5 @@
+package com.inc.fcr.car.enums;
+
+public enum RoofType {
+    CONVERTABLE, SUNROOF, MOONROOF, HARDTOP, PANORAMIC
+}
