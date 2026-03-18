f1826f0e731756554b22ddd26b444929e1a51ae3
AndrewSVO <andrewbgoldman@gmail.com>
2026-03-17 17:44:15 -0700

    implemented new Reservation class, User class with Address and DriversLicense class (attributes  for user and reservation)

---
+++ back/src/main/java/com/inc/fcr/reservation/Reservation.java
@@ -0,0 +1,61 @@
+package com.inc.fcr.reservation;
+
+import com.inc.fcr.car.Car;
+import com.inc.fcr.user.User;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "reservations")
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reservationId;
+    @ManyToOne
+    @JoinColumn(name = "vin", nullable = false)
+    private Car car;
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+    @Column(nullable = false)
+    private long pickupTime;
+    @Column(nullable = false)
+    private long dropOffTime;
+
+    public Reservation(Car car, User user, long pickupTime, long dropOffTime) {
+        this.car = car;
+        this.user = user;
+        this.pickupTime = pickupTime;
+        this.dropOffTime = dropOffTime;
+    }
+
+    public Reservation() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public long getPickupTime() {
+        return pickupTime;
+    }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public void setPickupTime(long pickupTime) {
+        this.pickupTime = pickupTime;
+    }
+
+    public long getDropOffTime() {
+        return dropOffTime;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setDropOffTime(long dropOffTime) {
+        this.dropOffTime = dropOffTime;
+    }
+}
