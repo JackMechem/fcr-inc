8019f14a03ae0d751024e79ce741e592196536a1
Jenny <jenny.ventura.447@my.csun.edu>
2026-04-02 16:38:50 -0700

    I think it's all done this time, added other errors, fixed 302

--- back/src/main/java/com/inc/fcr/errorHandling/ApiErrors.java
+++ back/src/main/java/com/inc/fcr/errorHandling/ApiErrors.java
@@ -10,20 +10,28 @@ import com.inc.fcr.car.enums.*;
 
 public final class ApiErrors {
 
-    public static void reDirectError(Context ctx) { // 300
-        ctx.status(302).header("Location", "/new-location");
+    private static void reDirectError(Context ctx, String newLocation) { // 302
+        ctx.status(302).header("Location", newLocation);
     }
 
-    public static void badRequest(Context ctx, Exception e) { // 400
-        ctx.status(400).json(new ApiErrorResponse(400, "Bad Request", "" + e, stackTraceString(e)));
+    private static void formatError(Context ctx, Exception e) { // 400
+        ctx.status(400).json(new ApiErrorResponse(400, "Improper Object Format", "" + e, stackTraceString(e)));
     }
 
-    public static void notFound(Context ctx) { // 400
-        ctx.status(404).json(new ApiErrorResponse(404, "Not Found", null, null));
+    private static void queryParamError(Context ctx, Exception e) { // 400
+        ctx.status(400).json(new ApiErrorResponse(400, "Invalid Query Parameters", "" + e, stackTraceString(e)));
     }
 
-    public static void serverError(Context ctx, Exception e) { // 500
+    private static void notFound(Context ctx) { // 404
+        ctx.status(404).json(new ApiErrorResponse(404, "Object Not Found", null, null));
+    }
+
+    private static void serverError(Context ctx, Exception e) { // 500
         ctx.status(500).json(new ApiErrorResponse(500, "Server Error", "" + e, stackTraceString(e)));
+    }
+
+    private static void databaseError(Context ctx, Exception e) { // 500
+        ctx.status(500).json(new ApiErrorResponse(500, "Database Error", "" + e, stackTraceString(e)));
     }
 
     private static String stackTraceString(Exception e) {
